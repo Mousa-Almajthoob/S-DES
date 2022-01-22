3c77f4bf54227cad745f5c7562574846f6a61e5d
Mousa Almajthoob <89401263+Mousa-Almajthoob@users.noreply.github.com>
2022-01-22 03:54:38 +0200

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,124 @@
+﻿Public Class Form1
+
+
+    Dim x As String
+    Dim Lk As String 'Lk= left k
+    Dim Rk As String    'Rk= Right key 
+    Dim LSk As String 'LSk= left(left sh) k
+    Dim RSk As String    'RSk= Right(left sh) key 
+    Dim P8o As String '' (6,3,7,4,8,5,10,9)
+    Dim k1 = P8o
+    Dim LS_2 As String    'LSk= left(2left shift) Lkey
+    Dim RS_2 As String    'RSk= Right(2left shift) Rkey 
+    Dim P8t As String '' permitation eight (6,3,7,4,8,5,10,9)
+    Dim k2 = P8t
+    Dim cont1 As String
+    Dim cont2 As String
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        x = t1.Text
+
+        Dim P10 As String   'P10 = permutation ten one  (3,5,2,7,4,10,1,9,8,6)
+        P10 = (x.Chars(2) & x.Chars(4) & x.Chars(1) & x.Chars(6) & x.Chars(3) & x.Chars(9) & x.Chars(0) & x.Chars(8) & x.Chars(7) & x.Chars(5))
+
+
+
+
+        '  Dim Lk As String    'Lk= left k
+        '  Dim Rk As String    'Rk= Right key 
+        Lk = (P10.Chars(0) & P10.Chars(1) & P10.Chars(2) & P10.Chars(3) & P10.Chars(4))
+        Rk = (P10.Chars(5) & P10.Chars(6) & P10.Chars(7) & P10.Chars(8) & P10.Chars(9))
+
+
+
+
+
+        'Dim LSk As String    'LSk= left(left sh) Lkey
+        'Dim RSk As String    'RSk= Right(left sh) Rkey 
+        LSk = (Lk.Chars(1) & Lk.Chars(2) & Lk.Chars(3) & Lk.Chars(4) & Lk.Chars(0))
+        RSk = (Rk.Chars(1) & Rk.Chars(2) & Rk.Chars(3) & Rk.Chars(4) & Rk.Chars(0))
+
+
+        cont1 = LSk & RSk
+
+
+
+
+
+        'P8o = permutation eight one (6,3,7,4,8,5,10,9) 
+        P8o = (cont1.Chars(5) & cont1.Chars(2) & cont1.Chars(6) & cont1.Chars(3) & cont1.Chars(7) & cont1.Chars(4) & cont1.Chars(9) & cont1.Chars(8))
+
+        t2.Text = P8o
+
+        Dim LLS_2 As String    'LSk= left(2left shift) Lkey
+        Dim RRS_2 As String    'RSk= Right(2left shift) Rkey 
+
+        LLS_2 = (LSk.Chars(2) & LSk.Chars(3) & LSk.Chars(4) & LSk.Chars(0) & LSk.Chars(1))
+        RRS_2 = (RSk.Chars(2) & RSk.Chars(3) & RSk.Chars(4) & RSk.Chars(0) & RSk.Chars(1))
+
+
+
+        cont2 = LLS_2 & RRS_2
+
+        P8t = (cont2.Chars(5) & cont2.Chars(2) & cont2.Chars(6) & cont2.Chars(3) & cont2.Chars(7) & cont2.Chars(4) & cont2.Chars(9) & cont2.Chars(8))
+
+        t3.Text = P8t
+
+
+
+    End Sub
+
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+
+        Me.Hide()
+        Form2.Show()
+
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        Me.Hide()
+        Form3.Show()
+
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If (OpenFileDialog1.ShowDialog = DialogResult.OK) Then
+            t1.Text = My.Computer.FileSystem.ReadAllText(OpenFileDialog1.FileName)
+        End If
+
+        If OpenFileDialog1.FileName = "" Then
+            Exit Sub
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+
+        Dim writer As New System.IO.StreamWriter("C:\Folder\key1.txt")
+
+        writer.Write(t2.Text)
+        writer.Close()
+        MessageBox.Show("Exported")
+
+
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+
+        Dim writer As New System.IO.StreamWriter("C:\Folder\key2.txt")
+
+        writer.Write(t3.Text)
+        writer.Close()
+        MessageBox.Show("Exported")
+
+
+    End Sub
+End Class
---
+++ Form3.Designer.vb
@@ -0,0 +1,228 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form3
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.t2 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.t1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.t4 = New System.Windows.Forms.TextBox()
+        Me.t3 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.t5 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        't2
+        '
+        Me.t2.Location = New System.Drawing.Point(132, 262)
+        Me.t2.Multiline = True
+        Me.t2.Name = "t2"
+        Me.t2.Size = New System.Drawing.Size(283, 132)
+        Me.t2.TabIndex = 11
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(12, 261)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(101, 20)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "The plaintext"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(175, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(122, 30)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Decryption"
+        '
+        't1
+        '
+        Me.t1.Location = New System.Drawing.Point(132, 83)
+        Me.t1.Multiline = True
+        Me.t1.Name = "t1"
+        Me.t1.Size = New System.Drawing.Size(283, 122)
+        Me.t1.TabIndex = 8
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(12, 86)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(114, 20)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "The cyphertext"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.Location = New System.Drawing.Point(175, 223)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(177, 33)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Cyphertext to plaintext"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button2.Location = New System.Drawing.Point(12, 546)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(101, 33)
+        Me.Button2.TabIndex = 12
+        Me.Button2.Text = "Back"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label5.Location = New System.Drawing.Point(366, 551)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(49, 20)
+        Me.Label5.TabIndex = 16
+        Me.Label5.Text = "KEY 2"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label4.Location = New System.Drawing.Point(366, 497)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(49, 20)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "KEY 1"
+        '
+        't4
+        '
+        Me.t4.Location = New System.Drawing.Point(421, 551)
+        Me.t4.Name = "t4"
+        Me.t4.Size = New System.Drawing.Size(283, 23)
+        Me.t4.TabIndex = 14
+        '
+        't3
+        '
+        Me.t3.Location = New System.Drawing.Point(421, 498)
+        Me.t3.Name = "t3"
+        Me.t3.Size = New System.Drawing.Size(283, 23)
+        Me.t3.TabIndex = 13
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button3.Location = New System.Drawing.Point(421, 102)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(177, 33)
+        Me.Button3.TabIndex = 18
+        Me.Button3.Text = "Browse"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        't5
+        '
+        Me.t5.Location = New System.Drawing.Point(636, 261)
+        Me.t5.Multiline = True
+        Me.t5.Name = "t5"
+        Me.t5.Size = New System.Drawing.Size(283, 132)
+        Me.t5.TabIndex = 19
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button4.Location = New System.Drawing.Point(440, 311)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(177, 33)
+        Me.Button4.TabIndex = 20
+        Me.Button4.Text = "Binary to text"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button5.Location = New System.Drawing.Point(693, 399)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(177, 33)
+        Me.Button5.TabIndex = 21
+        Me.Button5.Text = "Export"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Form3
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(931, 587)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.t5)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.t4)
+        Me.Controls.Add(Me.t3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.t2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.t1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form3"
+        Me.Text = "Form3"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents t2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents t1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents t4 As TextBox
+    Friend WithEvents t3 As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents t5 As TextBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+End Class
